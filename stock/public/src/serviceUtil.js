43e1b528a988efad3fe1c37632a8dc2fa0ae586b
jeonyeonkyu <ehb6915@naver.com>
2021-03-16 15:37:01 +0900

    Add: getResponseJsonUrl 추가

--- stock/public/src/serviceUtil.js
+++ stock/public/src/serviceUtil.js
@@ -1,3 +1,4 @@
 const delay = (data, ms) => new Promise(resolve => setTimeout(() => resolve(data), ms));
+const getResponseJsonUrl = async (url) => (await fetch(url)).json();
 
-export { delay };
+export { delay, getResponseJsonUrl };
