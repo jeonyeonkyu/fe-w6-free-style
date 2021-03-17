ed34adacccfab5bb7456cf716f2c92e0b711b782
jeonyeonkyu <ehb6915@naver.com>
2021-03-17 18:51:28 +0900

    Add: addEventlistener 추가

--- stock/public/src/util.js
+++ stock/public/src/util.js
@@ -1,7 +1,7 @@
 const _ = {
   $: (selector, base = document) => base.querySelector(selector),
   $All: (selector, base = document) => base.querySelectorAll(selector),
-  on: (selector, eventName, callback) => selector.addEventListener(eventName, callback),
+  on: (selector, eventName, callback, option) => selector.addEventListener(eventName, callback, option),
 }
 
 export default _;
