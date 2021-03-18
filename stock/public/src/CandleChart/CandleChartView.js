3ec799b707152a12ad7d5a857d897d470e9d621d
jeonyeonkyu <ehb6915@naver.com>
2021-03-18 22:09:57 +0900

    Fix: 옵션 수정하여 필요없는 함수 제거

--- stock/public/src/CandleChart/CandleChartView.js
+++ stock/public/src/CandleChart/CandleChartView.js
@@ -1,36 +1,53 @@
-import fetch from 'cross-fetch';
+import _ from '../util.js'
 import * as echarts from 'echarts';
-import { splitData } from './candleChartUtil.js';
-import { basicOption, makeXAxis, makeSeries, dispatchActionOption } from './candleChartOption.js';
+import { splitData, xhrRequest } from './candleChartUtil.js';
+import { basicOption, makeXAxis, makeSeries } from './candleChartOption.js';
 
 class CandleChartView {
-  constructor({ url, $boxChart }) {
+  constructor({ url, $receiveInput, $receiveButton, $boxChartTerm, $boxChart, today }) {
     this.url = url;
+    this.$receiveInput = $receiveInput;
+    this.$receiveButton = $receiveButton;
+    this.$boxChartTerm = $boxChartTerm;
     this.$boxChart = $boxChart;
+    this.today = today;
     this.init();
   }
 
   init() {
-    this.render();
+    this.initEvent();
   }
 
-  render() {
-    // const url = `${this.url}?requestType=1&timeframe=day&startTime=20210103&endTime=20210208&symbol=035720`
-    const ROOT_PATH = 'https://echarts.apache.org/examples';
+  initEvent() {
+    _.on(this.$receiveButton, 'click', this.radioBoxOriginalStateHandler.bind(this));
+    _.on(this.$receiveButton, 'click', this.renderChart.bind(this, this.$receiveInput.value, 'day'));
+    _.on(this.$boxChartTerm, 'click', this.changeRadioButtonHandler.bind(this));
+  }
+
+  changeRadioButtonHandler({ target }) {
+    if (target.tagName !== 'LABEL') return;
+    target.previousElementSibling.checked = 'checked';
+    this.renderChart(this.$receiveInput.value, target.id);
+  }
+
+  radioBoxOriginalStateHandler() {
+    _.$('#day', this.$boxChartTerm).checked = 'checked';
+  }
+
+  renderChart(tickerCode, term) {
+    const url = `${this.url}?requestType=1&timeframe=${term}&startTime=20140103&endTime=${this.today}&symbol=${tickerCode}`;
     const myChart = echarts.init(this.$boxChart);
     let option;
 
-    fetch(ROOT_PATH + '/data/asset/data/stock-DJI.json')
-      .then(data => data.json())
-      .then((rawData) => {
-        const data = splitData(rawData);
-        option = { ...basicOption, xAxis: makeXAxis(data), series: makeSeries(data) };
-        myChart.setOption(option, true);
-        myChart.dispatchAction(dispatchActionOption);
-      });
+    xhrRequest(url, (rawData) => {
+      const data = splitData(rawData);
+      option = { ...basicOption, xAxis: makeXAxis(data), series: makeSeries(data) };
+      myChart.setOption(option, true);
+    });
 
     option && myChart.setOption(option);
   }
+
 }
 
 export default CandleChartView;
