eee642ac4f7887f2c22e6f757b9dbb7d641fe1ab
jeonyeonkyu <ehb6915@naver.com>
2021-03-18 22:04:35 +0900

    Add: util 함수 추가

--- stock/public/src/util.js
+++ stock/public/src/util.js
@@ -2,6 +2,19 @@ const _ = {
   $: (selector, base = document) => base.querySelector(selector),
   $All: (selector, base = document) => base.querySelectorAll(selector),
   on: (selector, eventName, callback, option) => selector.addEventListener(eventName, callback, option),
+  genEl(tagName, { classNames, template, attributes } = {}) {
+    const $el = document.createElement(tagName);
+    if (classNames) $el.classList.add(...classNames);
+    if (template) $el.innerHTML = template;
+    if (attributes) Object.entries(attributes).forEach(([k, v]) => $el.setAttribute(k, v));
+    return $el;
+  },
+  show($target) {
+    $target.hidden = false;
+  },
+  hide($target) {
+    $target.hidden = true;
+  }
 }
 
 export default _;
